945b17230e2ded8a3ff61a59d8c122a10dd6a2d9
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-09-13 00:05:28 +0800

    add tokio

--- examples/server.rs
+++ examples/server.rs
@@ -1,11 +1,15 @@
 use pki_types::{CertificateDer, PrivateKeyDer};
 use rustls::crypto::CryptoProvider;
-use rustls::server::{Acceptor, ClientHello, ResolvesServerCert};
+use rustls::server::{ClientHello, ResolvesServerCert};
 use rustls::{sign, ServerConfig};
 use rustls_provider_rustcrypto::sign::ecdsa::EcdsaSigningKey;
 use rustls_provider_rustcrypto::Provider;
-use std::io::Write;
+use std::io::{self};
+use std::net::ToSocketAddrs;
 use std::sync::Arc;
+use tokio::io::{copy, sink, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio_rustls::TlsAcceptor;
 
 struct TestResolvesServerCert(Arc<sign::CertifiedKey>);
 
@@ -61,8 +65,8 @@ impl TestPki {
         }
     }
 
-    fn server_config<C: CryptoProvider>(&self) -> Arc<ServerConfig<C>> {
-        let mut server_config: ServerConfig<C> = ServerConfig::builder()
+    fn server_config<C: CryptoProvider>(&self) -> Arc<ServerConfig> {
+        let mut server_config: ServerConfig = ServerConfig::builder_with_provider(&Provider)
             .with_safe_defaults()
             .with_no_client_auth()
             .with_cert_resolver(Arc::new(TestResolvesServerCert::new(
@@ -76,50 +80,48 @@ impl TestPki {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
     env_logger::init();
 
     let pki = TestPki::new();
     let server_config = pki.server_config::<Provider>();
+    let addr = "0.0.0.0:4443"
+        .to_socket_addrs()?
+        .next()
+        .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::AddrNotAvailable))?;
+    let acceptor = TlsAcceptor::from(server_config);
 
-    let listener = std::net::TcpListener::bind(format!("0.0.0.0:{}", 4443)).unwrap();
-    'accept: for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        let mut acceptor = Acceptor::default();
+    let listener = TcpListener::bind(&addr).await?;
 
-        let accepted = loop {
-            if let Ok(_) = acceptor.read_tls(&mut stream) {
-                if let Ok(Some(accepted)) = acceptor.accept() {
-                    break accepted;
-                }
-            }
+    loop {
+        let (stream, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
 
-            eprintln!("unexpected connection");
-            continue 'accept;
+        let fut = async move {
+            let mut stream = acceptor.accept(stream).await?;
+
+            let mut output = sink();
+            stream
+                .write_all(
+                    &b"HTTP/1.0 200 ok\r\n\
+                Connection: close\r\n\
+                Content-length: 12\r\n\
+                \r\n\
+                Hello world!"[..],
+                )
+                .await?;
+            stream.shutdown().await?;
+            copy(&mut stream, &mut output).await?;
+            println!("Hello: {}", peer_addr);
+
+            Ok(()) as io::Result<()>
         };
 
-        match accepted.into_connection(server_config.clone()) {
-            Ok(mut conn) => {
-                let msg = concat!(
-                    "HTTP/1.1 200 OK\r\n",
-                    "Connection: Closed\r\n",
-                    "Content-Type: text/html\r\n",
-                    "\r\n",
-                    "<h1>Hello World!</h1>\r\n"
-                )
-                .as_bytes();
-
-                let _ = conn.writer().write(msg);
-                let _ = conn.write_tls(&mut stream);
-                _ = conn.complete_io(&mut stream);
-
-                conn.send_close_notify();
-                let _ = conn.write_tls(&mut stream);
-                _ = conn.complete_io(&mut stream);
+        tokio::spawn(async move {
+            if let Err(err) = fut.await {
+                eprintln!("{:?}", err);
             }
-            Err(e) => {
-                eprintln!("{}", e);
-            }
-        }
+        });
     }
 }
