c6131e66981d8a2878092b407d736969ca46ccc0
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-09-14 19:18:06 +0800

    explain why the payload splcing is done later in aead

--- src/aead.rs
+++ src/aead.rs
@@ -317,6 +317,10 @@ where
             .map_err(|_| rustls::Error::DecryptError)?;
             tag_pos
         };
+
+        // We defer the truncation to here, because we may inadverntly shifted the original data if the decryption failed.
+        // Another way to avoid this is to clone the payload slice starting after the explicit nonce, 
+        // but this will cause an additional cloning and copying
         payload.rotate_left(8);
         payload.truncate(tag_pos);
         Ok(m.into_plain_message())
