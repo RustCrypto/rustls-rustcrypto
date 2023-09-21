db9b8c8e1eda983ef6e4867ace6ac3aa9316c089
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-09-21 21:26:54 +0800

    add NoopServerVerifier

--- examples/client.rs
+++ examples/client.rs
@@ -2,7 +2,63 @@ use std::str::FromStr;
 
 use anyhow::anyhow;
 use hyper::{body::to_bytes, client, Body, Uri};
+use pki_types::CertificateDer;
+use rustls::{
+    client::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier},
+    DigitallySignedStruct, ServerName, SignatureScheme,
+};
 use rustls_provider_rustcrypto::Provider;
+
+struct NoopServerVerifier;
+
+impl ServerCertVerifier for NoopServerVerifier {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName,
+        _ocsp_response: &[u8],
+        _now: pki_types::UnixTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        vec![
+            SignatureScheme::RSA_PKCS1_SHA1,
+            SignatureScheme::ECDSA_SHA1_Legacy,
+            SignatureScheme::RSA_PKCS1_SHA256,
+            SignatureScheme::ECDSA_NISTP256_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384,
+            SignatureScheme::ECDSA_NISTP384_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512,
+            SignatureScheme::ECDSA_NISTP521_SHA512,
+            SignatureScheme::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512,
+            SignatureScheme::ED25519,
+            SignatureScheme::ED448,
+        ]
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -31,7 +87,7 @@ async fn main() -> anyhow::Result<()> {
     // stdout.
     let fut = async move {
         let res = client
-            .get(Uri::from_str("https://sha256.badssl.com/")?)
+            .get(Uri::from_str("https://ecc256.badssl.com/")?)
             .await
             .map_err(|e| anyhow!("Could not get: {:?}", e))?;
         println!("Status:\n{}", res.status());
