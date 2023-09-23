dcd897f57d76287dc5239f5fc0f195ae8c252284
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-09-23 18:34:35 +0800

    remove no_std limitation but add clippy guard

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-#![deny(clippy::all, clippy::pedantic)]
+#![deny(
+    clippy::all,
+    clippy::pedantic,
+    clippy::alloc_instead_of_core,
+    clippy::std_instead_of_alloc,
+    clippy::std_instead_of_core
+)]
 
 extern crate alloc;
 
