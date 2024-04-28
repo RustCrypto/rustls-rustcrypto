bcb4410b6409e2e8aaa149af0c244585ec672de4
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-04-28 10:25:37 +1000

    Remove TODO

--- validation/local_ping_pong_openssl/build.rs
+++ validation/local_ping_pong_openssl/build.rs
@@ -2,28 +2,20 @@ use std::fs::metadata;
 use std::path::Path;
 use std::process::Command;
 use std::time::SystemTime;
-//TODO: use library eventually
-//use openssl::rsa::Rsa;
 
 fn main() {
     check_gen_certs();
 }
 
-fn cargo_target_dir() -> String {
+fn manifest_dir() -> String {
     match std::env::var("CARGO_MANIFEST_DIR").as_deref() {
         Ok(target_dir) => target_dir.into(),
         _ => panic!("CARGO_MANIFEST_DIR required for build."),
     }
-    /* TODO: use target directory when generating w/o shell
-    match std::env::var("OUT_DIR").as_deref() {
-        Ok(target_dir) => target_dir.into(),
-        _ => panic!("OUT_DIR required for build."),
-    }
-    */
 }
 
 fn check_gen_certs() {
-    let out_dir_str = cargo_target_dir();
+    let out_dir_str = manifest_dir();
     let out_dir = Path::new(&out_dir_str).join("certs");
 
     let ca_crt_path = out_dir.join("ca.rsa4096.crt");
@@ -52,13 +44,4 @@ fn check_gen_certs() {
             .spawn()
             .expect("make rsa4096");
     }
-
-    // public exponent will be 65537
-    /* TODO: remove shell requirement:
-    let rsa = Rsa::generate(4096).expect("openssl genrsa rsa");
-    let ca_rsa = Rsa::generate(4096).expect("openssl genrsa ca_rsa");
-
-    let rsa_pem = rsa.private_key_to_pem().expect("rsa private_key_to_pem");
-    let ca_rsa_pem = ca_rsa.private_key_to_pem().expect("ca_rsa private_key_to_pem");
-    */
 }
