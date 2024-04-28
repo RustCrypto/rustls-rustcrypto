f3cf65f5156f0b7e25ced944243ebbb8207a4573
pinkforest <36498018+pinkforest@users.noreply.github.com>
2024-04-28 18:07:25 +1000

    No support for curve448

--- validation/local_ping_pong_openssl/src/lib.rs
+++ validation/local_ping_pong_openssl/src/lib.rs
@@ -130,6 +130,13 @@ mod test {
         group_list.X25519 = true;
         vs_openssl_as_client(group_list, OpenSslCipherSuites::default());
     }
+    #[test]
+    #[should_panic] // no support
+    fn vs_openssl_as_client_group_x448() {
+        let mut group_list = OpenSslGroupsList::all_false();
+        group_list.X448 = true;
+        vs_openssl_as_client(group_list, OpenSslCipherSuites::default());
+    }
 
     fn vs_openssl_as_client(groups_list: OpenSslGroupsList, cipher_suites: OpenSslCipherSuites) {
         let path_certs = Path::new("certs");
